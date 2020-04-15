116716c86f65c159c6ab9f169f561da50823f47c
Stephen Morrissey <sjm3524@rit.edu>
2020-04-15 18:19:01 -0400

    Update searchprocess.php

--- Back-End/php/searchprocess.php
+++ Back-End/php/searchprocess.php
@@ -1,6 +1,6 @@
 <?php
 	$path = './';
-	require $path.'../../dbConnect.inc';
+	require $path.'../../../../dbConnect.inc';
 ?>
 <h2>Search Results</h2>
 
@@ -24,19 +24,34 @@ $interest =  test_input($_POST['searchInterest']) ;
 
 
 
-$sql = "SELECT * FROM InterestBase where Keyword=$interest";
+$sql = "SELECT * FROM InterestBase where Keyword='$interest'";
 $res = mysqli_query($mysqli, $sql);
 
 if ($res->num_rows > 0) {
     // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "UserID: " . $row["UserID"]. " - Keyword: " . $row["Keyword"]. " -Description: " . $row["Description"]. " - Type: ". $row["Type"] ."<br>";
+    while($row = $res->fetch_assoc()) {
+		$id = $row["UserID"];
+		$selectName = "SELECT * FROM UserBase where UserID='$id'";
+		$personInfo = mysqli_query($mysqli, $selectName);
+		$personArray = $personInfo->fetch_assoc();
+		
+		$name = $personArray["Name"];
+		$email = $personArray["email"];
+		$type = $personArray["isStudent"];
+		
+        echo "Name: " . $name. " - Keyword: " . $row["Keyword"]. " -Description: " . $row["Description"]. " - Type: ". $row["Type"] . " -Email: ". $email;
+		if ($type == 1){
+			echo " - Student <br>";
+		}
+		else {
+			echo " - Faculty <br>";
+		}
     }
 } else {
     echo "0 results";
 }
 
-
+?>
 
 <?php
 	

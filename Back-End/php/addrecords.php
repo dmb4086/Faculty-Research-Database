726f5bc97fa321863706dbdc1edd82fad643790a
Dev <dmb4086@rit.edu>
2020-04-19 18:58:55 -0400

    all tables populated

--- Back-End/php/addrecords.php
+++ Back-End/php/addrecords.php
@@ -1,9 +1,9 @@
 <?php
 
- $path = './';
- require $path.'../../../../dbConnect.inc';
+$path = './';
+require $path . '../../../../dbConnect.inc';
 
-if($mysqli){
+if ($mysqli) {
 
 
     if (isset($_GET['name'])) {
@@ -14,28 +14,28 @@ if($mysqli){
         $description = $_GET['description'];
         $StudentFlag = $_GET['StudentFlag'];
         $type = $_GET['type'];
+        $department = $_GET['department'];
 
         // setting the value for StudentFlag to correspond to the databse as the database accepts 
         // 1 or 0 for true or false
         if ($StudentFlag == "true") {
             $StudentFlag = 1;
-        }
-        else{
-            $StudentFlag = 0; 
+        } else {
+            $StudentFlag = 0;
         }
 
 
         // echo "name: ".$name." Email: ".$email." keyword: ".$keyword. "type: ".$type. "SF". $StudentFlag;
 
-        $stmt=$mysqli->prepare("INSERT INTO UserBase (name, email, isStudent) VALUES (?,?,?)");
-        $stmt->bind_param("ssi", $name,$email,$StudentFlag);
+        $stmt = $mysqli->prepare("INSERT INTO UserBase (name, email, isStudent) VALUES (?,?,?)");
+        $stmt->bind_param("ssi", $name, $email, $StudentFlag);
 
         $stmt->execute();
         $stmt->close();
     }
-        // UserBase has been populated with the selected record. 
+    // UserBase has been populated with the selected record. 
 
-        /*
+    /*
             @author Dev
             Now that we have populated UserBase we have to populate 
             associated tables, more specifically we have to determine whther the user is a 
@@ -65,38 +65,26 @@ if($mysqli){
 
 
         */
-        $sql = "SELECT * FROM UserBase where email='$email'";
-        $res = mysqli_query($mysqli, $sql);
-        while($row = $res->fetch_assoc()) {
-            $id = $row["UserID"];
-            $name = $row['Name'];
-            $StudentFlag = $row['isStudent'];
-        }
-        // echo "new echo: ID of ". $name. "is ". $id;
-        
-        // Step 2 
+    $sql = "SELECT * FROM UserBase where email='$email'";
+    $res = mysqli_query($mysqli, $sql);
+    while ($row = $res->fetch_assoc()) {
+        $id = $row["UserID"];
+        $name = $row['Name'];
+        $StudentFlag = $row['isStudent'];
+    }
+    // echo "new echo: ID of ". $name. "is ". $id;
 
-        // if($StudentFlag == 1){
-        //     $stmt=$mysqli->prepare("INSERT INTO StudentBase (id) VALUES (?)");
-        //     $stmt->bind_param("i", $id);
+    // Step 2 
 
-        // }
+    // if($StudentFlag == 1){
+    //     $stmt=$mysqli->prepare("INSERT INTO StudentBase (id) VALUES (?)");
+    //     $stmt->bind_param("i", $id);
 
-        // Step 3 
-        $stmt=$mysqli->prepare("INSERT INTO InterestBase (UserID, Keyword, Description,Type) VALUES (?,?,?,?)");
-        $stmt->bind_param("isss", $id,$keyword,$description,$type);
-        $stmt->execute();
-        $stmt->close();
+    // }
 
-
-
-
-
-
-
-
-
-        
+    // Step 3 
+    $stmt = $mysqli->prepare("INSERT INTO InterestBase (UserID, Keyword, Description,Type) VALUES (?,?,?,?)");
+    $stmt->bind_param("isss", $id, $keyword, $description, $type);
+    $stmt->execute();
+    $stmt->close();
 }
-
-?>
